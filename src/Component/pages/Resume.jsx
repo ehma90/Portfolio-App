790083e89ede2331da979ca82a19ff655ec9fe68
Emmanuel Essien <ehmaessien@gmail.com>
2022-12-07 20:34:21 +0100

    Update Resume page

--- src/Component/pages/Resume.jsx
+++ src/Component/pages/Resume.jsx
@@ -12,20 +12,28 @@ function Resume() {
       </div>
 
       <div className="container mx-auto">
-        <h1 className="experience-header">Experience</h1>
+        <h2 className="resume-header">Experience</h2>
         <div className="experience">
           <div className="resume-list">
-            <label className="experience-date">Oct 2022 - Present</label>
-            <label className="experience-date">Full-time</label>
+            <label className="resume-date">Oct 2022 - Present</label>
+            <label className="resume-date">Full-time</label>
           </div>
           <div className="resume-list">
             <label className="FED">
-              Front-end Developer <span>at Rareket Technology Solution (Lagos, Nigeria)</span>{" "}
+              Front-end Developer{" "}
+              <span>at Rareket Technology Solution (Lagos, Nigeria)</span>{" "}
             </label>
             <ul>
-              <div className="title">Assigned to develop frontend of a web3 application which inlcude features such as:</div>
+              <div className="title">
+                Assigned to develop frontend of a web3 application which inlcude
+                features such as:
+              </div>
               <li>
-              Connect wallet modal to interact with the blockchain services
+                Built connect wallet modal to interact with the Blockchain smart
+                contracts like Ethereum and Polygon.
+              </li>
+              <li>
+                Built Explore page that shows collections of an Artist NFTs.
               </li>
             </ul>
           </div>
@@ -33,8 +41,8 @@ function Resume() {
 
         <div className="experience">
           <div className="resume-list">
-            <label className="experience-date">Feb 2022 - Jun 2022</label>
-            <label className="experience-date">Full-time</label>
+            <label className="resume-date">Feb 2022 - Jun 2022</label>
+            <label className="resume-date">Full-time</label>
           </div>
           <div className="resume-list">
             <label className="FED">
@@ -47,6 +55,69 @@ function Resume() {
               </li>
             </ul>
           </div>
+        </div>
+
+        <h2 className="resume-header">Education</h2>
+        <div className="education">
+          <div className="resume-list">
+            <label className="resume-date">Mar, 2015 - October, 2018</label>
+          </div>
+          <div className="resume-list">
+            <label className="FED">
+              B.Sc. in Physics{" "}
+              <span>
+                at University of Uyo, <br /> (Akwa-Ibom, Nigeria)
+              </span>
+            </label>
+          </div>
+        </div>
+
+        <h2 className="resume-header">Skills and Tech Stack</h2>
+        <div className=" stack">
+          <ul>
+            <li>
+              CSS libraries and tools including Tailwind CSS, Bootstrap, Sass.
+            </li>
+            <li>JavaScript frameworks such as: React.js.</li>
+            <li>JavaScript meta frameworks such as: Next.js.</li>
+            <li>Design tools like Figma.</li>
+          </ul>
+        </div>
+
+        <h2 className="resume-header">Contact Information</h2>
+        <div className="contact">
+          <ul>
+            <li>
+              Email:{" "}
+              <span>
+                <a href="mailto:ehmaessien@gmail.com">ehmaessien@gmail.com</a>
+              </span>
+            </li>
+            <li>
+              Twitter:{" "}
+              <span>
+                <a href="https://twitter.com/ehmaessieen">@ehmaessieen</a>
+              </span>
+            </li>
+            <li>
+              LinkedIn:{" "}
+              <span>
+                <a href="https://linkedin.com/in/ehmaessien">@ehmaessien</a>
+              </span>
+            </li>
+            <li>
+              Github:{" "}
+              <span>
+                <a href="https://github.com/ehma90">@ehma90</a>
+              </span>
+            </li>
+            <li>
+              Telegram:{" "}
+              <span>
+                <a href="https://t.me/ehma90">@ehmaessien</a>
+              </span>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
