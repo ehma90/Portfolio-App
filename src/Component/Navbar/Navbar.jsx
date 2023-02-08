c3183acafa988affd01f0f6f073069b7a6dd13b3
Emmanuel Essien <ehmaessien@gmail.com>
2023-02-08 09:32:35 +0100

    Update navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -11,17 +11,17 @@ function Navbar() {
     <div className="container my-6">
       <nav className=" flex justify-center  md:justify-end gap-x-8">
         <NavLink to="/">
-          <p className={splitLocation[1] === '' ? 'text-lg md:text-xl text-green-900 underline decoration-1' : 'text-lg md:text-xl'} >
+          <p className={splitLocation[1] === '' ? 'text-lg md:text-xl text-green-900 border-b border-green-900 decoration-1' : 'text-lg md:text-xl'} >
             Home
           </p>
         </NavLink>
         <NavLink to="/projects">
-          <p className={splitLocation[1] === 'projects' ? 'text-lg md:text-xl text-green-900 underline decoration-1' : 'text-lg md:text-xl'}>
+          <p className={splitLocation[1] === 'projects' ? 'text-lg md:text-xl text-green-900 border-b border-green-900 decoration-1' : 'text-lg md:text-xl'}>
             Projects
           </p>
         </NavLink>
         <NavLink to="/resume">
-          <p className={splitLocation[1] === 'resume' ? 'text-lg md:text-xl text-green-900 underline decoration-1' : 'text-lg md:text-xl'}>
+          <p className={splitLocation[1] === 'resume' ? 'text-lg md:text-xl text-green-900 border-b border-green-900 decoration-1' : 'text-lg md:text-xl'}>
             Resume
           </p>
         </NavLink>
