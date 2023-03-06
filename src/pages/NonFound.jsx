5783de3f5d958fecace10e7cc923206f3e6cde12
Emmanuel Essien <ehmaessien@gmail.com>
2023-03-06 02:26:57 +0100

    Update notfound

--- src/pages/NonFound.jsx
+++ src/pages/NonFound.jsx
@@ -2,9 +2,12 @@ import {Link} from 'react-router-dom'
 
 function NonFound() {
   return (
-    <div className='w-[100vw] h-[100vh] flex flex-col justify-center items-center text-center text-xl md:text-[40px]'>
+    <div className='h-[70vh] flex flex-col justify-center items-center text-center gap-6 text-xl md:text-[40px]'>
       <h2 className='font-bold my-3'>Oops</h2>
-      <p>Page Not Found</p>
+      <p >Page Not Found</p>
+      <Link to="/" className='text-white px-12 py-8 bg-green-900 rounded'>
+      Go back
+      </Link>
     </div>
   );
 }
